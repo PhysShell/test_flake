259499f9075079780b9d512c4a45571877c63d14
PhysShell <phillybo@yandex.kz>
2024-07-03 23:24:20 +0500

    first flake

--- flake.nix
+++ flake.nix
@@ -6,19 +6,14 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-defaultPackage.x86_64-linux =
-      # Notice the reference to nixpkgs here.
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "hello";
-        src = self;
-        buildPhase = "gcc -o hello ./hello.c";
-        installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
+    packages.x86_64-linux.default =
+        # Notice the reference to nixpkgs here.
+        with import nixpkgs { system = "x86_64-linux"; };
+        stdenv.mkDerivation {
+          name = "hello";
+          src = self;
+          buildPhase = "gcc -o hello ./hello.c";
+          installPhase = "mkdir -p $out/bin; install -t $out/bin hello";
       };
-#    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-#    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
